e4063027970385e768c94bc409a52fb227a97795
herald-noel <bontilao.heraldnoel@gmail.com>
2022-12-15 00:59:18 +0800

    Complete SimpleCalc

--- src/SimpleCalc/SimpleCalcGUI.java
+++ src/SimpleCalc/SimpleCalcGUI.java
@@ -1,6 +1,8 @@
 package SimpleCalc;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class SimpleCalcGUI extends JFrame {
     private JPanel pnlMain;
@@ -10,7 +12,50 @@ public class SimpleCalcGUI extends JFrame {
     private JTextField tfNumber2;
     private JTextField lblResult;
 
-    public static void main(String[] args) {
+    public SimpleCalcGUI() {
+        btnCompute.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                double a = toDouble(tfNumber1.getText());
+                double b = toDouble(tfNumber2.getText());
+                String op = (String) cbOperations.getSelectedItem();
+                assert op != null;
+                double answer = calc(a, b, op);
+                lblResult.setText(String.format("%.2f", answer));
+            }
+        });
+    }
 
+    public static void main(String[] args) {
+        SimpleCalcGUI app = new SimpleCalcGUI();
+        app.setContentPane(app.pnlMain);
+        app.setSize(550,300);
+        app.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        app.setTitle("Simple Calculator");
+        app.setLocationRelativeTo(null); // Centers the window upon running
+        app.setVisible(true);
+    }
+
+    public double toDouble (String n) {
+        return Double.parseDouble(n);
+    }
+
+    public double calc(double a, double b, String op) {
+        double answer = 0;
+        switch (op) {
+            case "+":
+                answer = a + b;
+                break;
+            case "-":
+                answer = a - b;
+                break;
+            case "*":
+                answer = a * b;
+                break;
+            case "/":
+                answer = a / b;
+                break;
+        }
+        return answer;
     }
 }
