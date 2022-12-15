7e94beb13aca18119c3bc3b2bef59c8e4b618e70
herald-noel <bontilao.heraldnoel@gmail.com>
2022-12-15 11:40:43 +0800

    Complete FoodOrdering

--- src/FoodOrdering/FoodOrderGUI.java
+++ src/FoodOrdering/FoodOrderGUI.java
@@ -1,6 +1,9 @@
 package FoodOrdering;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Hashtable;
 
 public class FoodOrderGUI extends JFrame {
     private JPanel pnlMain;
@@ -16,7 +19,69 @@ public class FoodOrderGUI extends JFrame {
     private JRadioButton rb10;
     private JRadioButton rb15;
 
+    private final Hashtable<JCheckBox, Integer> foods;
+    private final Hashtable<JRadioButton, Double> discounts;
+    public FoodOrderGUI() {
+        // Store all the orders in the foods hashtable
+        foods = new Hashtable<>();
+        foods.put(cPizza, 100);
+        foods.put(cBurger, 80);
+        foods.put(cFries, 65);
+        foods.put(cSoftDrinks, 55);
+        foods.put(cTea, 50);
+        foods.put(cSundae, 40);
+        // Store all the discount in the discounts hashtable
+        discounts = new Hashtable<>();
+        discounts.put(rbNone, 0.0);
+        discounts.put(rb5, 0.05);
+        discounts.put(rb10, 0.10);
+        discounts.put(rb15, 0.15);
+        // Set rbNone Default to true
+        rbNone.setSelected(true);
+        // Method when Order button is clicked
+        btnOrder.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                promptPrice();
+            }
+        });
+    }
     public static void main(String[] args) {
-
+        FoodOrderGUI app = new FoodOrderGUI();
+        app.setContentPane(app.pnlMain);
+        app.setSize(450, 550);
+        app.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        app.setTitle("Food Ordering System");
+        app.setLocationRelativeTo(null);
+        app.setVisible(true);
+    }
+    // Method that adds the selected food
+    public int sumOrder() {
+        int total = 0;
+        for (JCheckBox food : foods.keySet()) {
+            if (food.isSelected()) {
+                total += foods.get(food);
+                food.setSelected(false); // Uncheck the checkbox
+            }
+        }
+        return total;
+    }
+    // Method that returns the discount
+    public Double getDiscount() {
+        Double numDiscount = null;
+        for (JRadioButton discount : discounts.keySet()) {
+            if (discount.isSelected()) {
+                rbNone.setSelected(true); // Set selection of rbNone to true
+                numDiscount = discounts.get(discount);
+                break;
+            }
+        }
+        return numDiscount;
+    }
+    public void promptPrice() {
+        int total = sumOrder();
+        double discount = total * getDiscount();
+        double price = total - discount;
+        JOptionPane.showMessageDialog(pnlMain, String.format("The total price is Php %.2f", price));
     }
 }
